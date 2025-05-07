8da6e44ac6d20c3dfb14196bb08aaa765a76d5f3
samuel <samuel.gomes.k276@gmail.com>
2025-05-07 13:53:42 +0100

    Pagination explained

--- app/crud.py
+++ app/crud.py
@@ -4,14 +4,14 @@ from app.database import items_db
 from app.models import Item, ItemCreate, ItemUpdate
 
 """
-I would apply the following changes to the get_items function:
-1. Add Pagination: Introduce skip and limit parameters to support pagination.
+I would apply Pagination to the get_items function, introducing skip and limit parameters.
+This would allow the client to specify how many items to skip and how many to return.
 
 Explaination:
 - Because most of the time consumed is in constructing the response, 
 we can optimize the get_items function by adding pagination.
 - This will allow the client to request only a subset of items, 
-reducing the amount of data processed and sent over the network.
+reducing the amount of data processed and sent over the network
 - If there is still a need to fetch all items,
 the client can set skip to 0 and limit to a crazy large number.
 
@@ -21,11 +21,12 @@ the time it takes to process the request will be heavily reduced.
 Time comparison:
 - Before: O(n) for filtering and O(n) for constructing the response,
 - After: O(n) for filtering and O(1) for constructing the response.
+
+We could also use a pagination approach with page and size as parameters
+or a token-based pagination approach (using a cursor to track the last item seen)
 """
 def get_items(min_price: float = 0.0, skip: int = 0, limit: int = 100) -> List[Item]:
-   filtered_items = [item for item in items_db if item["price"] >= min_price]
-   paginated_items = filtered_items[skip: skip + limit]
-   return [Item(**item) for item in paginated_items]
+   return [Item(**item) for item in items_db[skip : skip + limit] if item["price"] >= min_price]
 
 
 def create_item(item: ItemCreate) -> Item:
